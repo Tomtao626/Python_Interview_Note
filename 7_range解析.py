f79dbf5203bdf83b72edec99f0f345a312d945e1
tomtao <751825253@qq.com>
2020-10-30 16:16:58 +0800

    python standards

---
+++ 7_range解析.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/env python
+# -*- coding:utf-8 _*-
+""" 
+@author:tom_tao626 
+@license: Apache Licence 
+@file: 7_range解析.py 
+@time: 2020/10/26
+@contact: tp320670258@gmail.com
+@site:  
+@software: PyCharm 
+"""
+
+
+
+
+
