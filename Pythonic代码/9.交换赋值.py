a9a0f1c36526be57b36292b12a18d20b318e750e
tomtao <751825253@qq.com>
2020-12-07 18:58:36 +0800

    bugfix

--- Pythonic代码/9.交换赋值.py
+++ Pythonic代码/9.交换赋值.py
@@ -3,17 +3,19 @@
 """ 
 @author:tom_tao626 
 @license: Apache Licence 
-@file: 8.使用语法糖.py 
+@file: 9.交换赋值.py 
 @time: 2020/12/07
 @contact: tp320670258@gmail.com
 @site: xxxx.suizhu.net
 @software: PyCharm 
 """
 
-# list() or []
+#  不推荐
+a = 1
+b = 2
+temp = a
+a = b
+b = a
 
-ls_1 = list()
-
-# fatser
-
-ls_2 = []
+#  推荐
+a, b = b, a  # 先生成一个元组(tuple)对象，然后unpack
--- 设计模式/3-建造者模式.py
+++ 设计模式/3-建造者模式.py
@@ -3,17 +3,10 @@
 """ 
 @author:tom_tao626 
 @license: Apache Licence 
-@file: 8.使用语法糖.py 
+@file: 3-建造者模式.py 
 @time: 2020/12/07
 @contact: tp320670258@gmail.com
 @site: xxxx.suizhu.net
 @software: PyCharm 
 """
 
-# list() or []
-
-ls_1 = list()
-
-# fatser
-
-ls_2 = []
